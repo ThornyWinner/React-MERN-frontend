c2b1bf255e5a49ed89f9242f72356bb2680f8f07
Héctor <hectordanielsalas@gmail.com>
2024-10-15 07:38:36 -0600

    Se excluye archivo .env

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,5 @@
 // Archivo de Barril
 
 export * from './calendarLocalizer';
+export * from './getEnvVariables';
 export * from './getMessages';
--- src/store/ui/uiSlice.js
+++ src/store/ui/uiSlice.js
@@ -1,7 +1,7 @@
 //* Estos action creators son exportados para ser utilizados en el código, por ejemplo, en el hook useUiStore que despacha estas acciones para abrir o cerrar el modal.
 //* createSlice: Esta función de Redux Toolkit es utilizada para crear un slice que contiene tanto el estado como los reducers que modifican su estado.
 //* - name: El nombre del slice, en este caso es 'ui'. Este nombre es importante porque se utiliza para nombrar el estado dentro del store global de Redux.
-//* - initialState: El estado inicial del slice, aquí se define que el modal (referido por isDateModalOpen) comienza como cerrado (valor false)-
+//* - initialState: El estado inicial del slice, aquí se define que el modal (referido por isDateModalOpen) comienza como cerrado (valor false).
 //* - reducers: Son las funciones que definen cómo cambia el estado cuando una acción es despachada. En este caso:
 //*     + onOpenDateModal: Cambia el estado a true, lo que significa que el modal está abierto.
 //*     + onCloseDateModal: Cambia el estado a false, lo que significa que el modal está cerrado.
